7d18bb45aea9cd5b50dd23cd0fbfa3482703e323
GuillePfarr <105952072+GuillePfarr@users.noreply.github.com>
2023-04-30 11:47:04 -0300

    Comienzo imágenes

--- PortfolioGPfarr/BackEnd/gui/src/main/java/com/portfoliogui/gui/Controller/PersonaController.java
+++ PortfolioGPfarr/BackEnd/gui/src/main/java/com/portfoliogui/gui/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/persona")/*quité s final--cambié persona por auth*/
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")/*faltaban dos puntos desp de http*/
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
@@ -33,7 +33,7 @@ public class PersonaController {
     
     @GetMapping("/detail/{id}")
     public ResponseEntity<Persona> getById(@PathVariable("id")int id){
-        if(!personaService.existsById(id)){/*aquí llave y cerraba en 44*/
+        if(!personaService.existsById(id)){/*faltaban las llaves*/
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
         }
         
--- PortfolioGPfarr/BackEnd/gui/src/main/java/com/portfoliogui/gui/Dto/dtoPersona.java
+++ PortfolioGPfarr/BackEnd/gui/src/main/java/com/portfoliogui/gui/Dto/dtoPersona.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfoliogui.gui.Dto;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author W10
- */
+
 public class dtoPersona {
     
     
